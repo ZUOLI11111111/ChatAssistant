97a7c2021f857496ca20fb5e1440db22b960a82d
zuoli <254054663@qq.com>
2025-08-02 21:24:15 +0800

    update Base

---
+++ app/tools/Base.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Optional
+from pydantic import BaseModel, Field
+
+class BaseTool(ABC, BaseModel):
+    name: str
+    description: str
+    parameters: Optional[dict] = None
+    
+    class  Config:
+        arbitrary_types_allowed = True
+    
+    @abstractmethod
+    async def execute(self, **kwargs) -> Any:
+        pass
+    def ToParam(self) -> Dict:
+        return {
+            "type": "function",
+            "function": {
+                "name": self.name,
+                "description": self.description,
+                "parameters":self.parameters,
+            },
+        }
+    
+class ToolResult(BaseModel):
+    output: Any = Field(default=None)
+    error: Optional[str] = Field(default=None)
+    base64_image: Optional[str] = Field(default=None)
+    system: Optional[str] = Field(default=None)
+
+    class Config:
+        arbitrary_types_allowed = True
+
+class CLIResult(ToolResult):
+    pass
+class ToolFailure(ToolResult):
+    pass
+
+   
